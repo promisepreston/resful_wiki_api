bc20fa0d542ec1993a9b80723e52b5fc4d7ddd8b
Promise Chukwuenyem <promisepreston@gmail.com>
2019-08-28 06:36:42 +0100

    edit the category controller file

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CategoryController < ApplicationController
   def show
-    category = params[:category] ? params[:category] : 'sports'
+    category = params[:category] || 'sports'
     @category = Category.where(cat_title: category.capitalize).first
 
     render json: @category,
