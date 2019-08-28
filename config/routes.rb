f53576dfbd021bddaaa98d53056aa45f53ecdd78
Promise Chukwuenyem <promisepreston@gmail.com>
2019-08-28 07:34:29 +0100

    add the graph controller, modify the links serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'graph/:graph', to: 'graph#show'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       get 'category/:category', to: 'category#show'
     end
   end
